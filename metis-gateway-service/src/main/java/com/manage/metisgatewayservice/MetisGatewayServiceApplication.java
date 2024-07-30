7b4c346301b09797097958099f0ebdddeb721dc2
stone <astrataraxia@gmail.com>
2024-07-30 23:09:05 +0900

    config, gateway, discovery
    msa 서버 구축

---
+++ metis-gateway-service/src/main/java/com/manage/metisgatewayservice/MetisGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manage.metisgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetisGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetisGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ metis-gateway-service/src/test/java/com/manage/metisgatewayservice/MetisGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manage.metisgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetisGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
